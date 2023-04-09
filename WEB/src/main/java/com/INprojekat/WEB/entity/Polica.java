67fe98d9de746142d7e89b494471f19826919838
Savo Savic <ssavic0120@gmail.com>
2023-04-09 20:57:11 +0200

    Adding getters and setters 2

--- WEB/src/main/java/com/INprojekat/WEB/entity/Polica.java
+++ WEB/src/main/java/com/INprojekat/WEB/entity/Polica.java
@@ -3,6 +3,17 @@ package com.INprojekat.WEB.entity;
 public class Polica {
     private String Naziv;
     private boolean primarna;
+    private Stavka_Police s_police;
+
+    public String getNaziv() { return Naziv; }
+
+    public void setNaziv(String naziv) { Naziv = naziv; }
+
+    public boolean isPrimarna() { return primarna; }
+
+    public void setPrimarna(boolean primarna) { this.primarna = primarna; }
+
+    public Stavka_Police getS_police() { return s_police; }
 
     public void setS_police(Stavka_Police s_police) { this.s_police = s_police; }
 }
