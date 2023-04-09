a78ec018b33c0c2589e53dde2534607ab29dec44
Savo Savic <ssavic0120@gmail.com>
2023-04-09 19:39:25 +0200

    Adding class Zanr

---
+++ WEB/src/main/java/com/INprojekat/WEB/entity/Zanr.java
@@ -0,0 +1,13 @@
+package com.INprojekat.WEB.entity;
+
+public class Zanr {
+    private String zanr_knjige;
+
+    public void setZanr(String zanr) {
+        this.zanr_knjige = zanr;
+    }
+
+    public String getZanr_knjige() {
+        return zanr_knjige;
+    }
+}
