53078bca084a222532e2a3d0092f579031eb97e4
Nikola Radojčić <nikradojcic@gmail.com>
2023-06-16 17:08:25 +0200

    Changing titles

--- FRONT/src/router/index.js
+++ FRONT/src/router/index.js
@@ -1,23 +1,122 @@
+<<<<<<< Updated upstream
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PretragaView from '../views/PretragaView.vue'
+=======
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeNeprijavljeniView from '../views/HomeNeprijavljeniView.vue';
+import HomeCitalacView from '../views/HomeCitalacView.vue';
+import HomeAutorView from '../views/HomeAutorView.vue';
+import HomeAdministratorView from '../views/HomeAdministratorView.vue';
+import PretragaNeprijavljeniView from '../views/PretragaNeprijavljeniView.vue';
+import PretragaCitalacView from '../views/PretragaCitalacView.vue';
+import PretragaAutorView from '../views/PretragaAutorView.vue';
+import PretragaAdministratorView from '../views/PretragaAdministratorView.vue';
+import LoginSection from '../components/LoginSection.vue';
+import RegisterSection from '../components/RegisterSection.vue';
+>>>>>>> Stashed changes
 
 const routes = [
   {
     path: '/',
     name: 'home',
+<<<<<<< Updated upstream
     component: HomeView
   },
   {
     path: '/pretraga',
     name: 'pretraga',
     component: PretragaView
+=======
+    component: HomeNeprijavljeniView,
+    meta: {
+      title: 'Home'
+    }
+  },
+  {
+    path: '/homeCitalac',
+    name: 'homeCitalac',
+    component: HomeCitalacView,
+    meta: {
+      title: 'Home'
+    }
+  },
+  {
+    path: '/homeAutor',
+    name: 'homeAutor',
+    component: HomeAutorView,
+    meta: {
+      title: 'Home'
+    }
+  },
+  {
+    path: '/homeAdministrator',
+    name: 'homeAdministrator',
+    component: HomeAdministratorView,
+    meta: {
+      title: 'Home'
+    }
+  },
+  {
+    path: '/pretragaNeprijavljeni',
+    name: 'pretragaNeprijavljeni',
+    component: PretragaNeprijavljeniView,
+    meta: {
+      title: 'Pretraga'
+    }
+  },
+  {
+    path: '/pretragaCitalac',
+    name: 'pretragaCitalac',
+    component: PretragaCitalacView,
+    meta: {
+      title: 'Pretraga'
+    }
+  },
+  {
+    path: '/pretragaAutor',
+    name: 'pretragaAutor',
+    component: PretragaAutorView,
+    meta: {
+      title: 'Pretraga'
+    }
+  },
+  {
+    path: '/pretragaAdministrator',
+    name: 'pretragaAdministrator',
+    component: PretragaAdministratorView,
+    meta: {
+      title: 'Pretraga'
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginSection,
+    meta: {
+      title: 'Pretraga'
+    }
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterSection,
+    meta: {
+      title: 'Pretraga'
+    }
+>>>>>>> Stashed changes
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const pageTitle = to.meta.title || 'My Website'; // Use the default title if no title is defined for the route
+  document.title = pageTitle;
+  next();
+});
+
+export default router;
---
+++ FRONT/src/views/HomeAdministratorView.vue
@@ -0,0 +1,230 @@
+<template>
+    <header>
+      <div class="pre-header">
+          <div class="container1">
+              <div class="row1">
+                  <div class="imageheader1">
+                      <div class="col-md-4">
+                          <img src="../assets/headerLeftRight.png">
+                      </div>
+                  </div>  
+                  <div class="imagelogo">
+                      <div class="col-md-4">
+                          <img src="../assets/logo.png">
+                      </div>
+                  </div> 
+                  <div class="imageheader2">
+                      <div class="col-md-4">
+                        <img src="../assets/headerLeftRight.png">
+                      </div>
+                  </div> 
+              </div>
+          </div>
+      </div>
+      <div class="container2">
+          <div class="row2">
+              <div>
+                  <ul class="menu">
+                      <li id="pocetna"><a href="/homeAdministrator">Početna</a></li>
+                      <li id="pretraga"><a href="/pretragaAdministrator">Pretraga 🔍</a></li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+  </header>
+  
+  <footer>
+      <p>&copy; 2023 BookBuddy. Sva prava zadržana.</p>
+  </footer>
+  </template>
+  
+  <script>
+  </script>
+  
+  <style>
+  * {
+      margin: 0;
+      padding: 0;
+  }
+  
+  .pre-header {
+      background-color: aquamarine;
+      height: 190px;
+  }
+  
+  .menu {
+      display: flex;
+      justify-content:space-around;
+      list-style: none;
+      padding: 0;
+    }
+    
+  a{
+      font-weight: bold;
+      font-style: italic;
+      font-size: 15px;
+      color: rgb(36, 136, 102);
+  }
+  
+  .container2 {
+      background-color: rgb(0, 255, 173);
+      height: 40px;
+      padding-top: 8px;
+  }
+  
+  .container1 .row1 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  }
+    
+  .container1 .col-md-4 {
+      flex: 1;
+  }
+  
+  .imagelogo img {
+      height: 90px;
+  }
+  
+  .container1 .col-md-4:nth-child(2) img {
+      display: block;
+      margin: 0 auto;
+  }
+  
+  .imageheader1 img {
+      opacity: 0.75;
+      padding-top: 15px;
+  }
+  
+  .imageheader2 {
+      opacity: 0.75;
+      padding-top: 15px;
+  }
+  
+  .pre-log {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  }
+  
+  .pre-reg {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  }
+  
+  h1 {
+      font-size: 60px;
+      margin-left: 15px;
+      font-weight: bold;
+  }
+  
+  h2 {
+      font-size: 60px;
+      margin-left:15px;
+      font-weight: bold;
+  }
+  
+  .container2 button{
+      font-weight: bold;
+      font-style: 8px;
+      font-size: 15px;
+      color: rgb(36, 136, 102);
+  }
+  
+  .row4 input {
+      border: 2px solid rgb(127, 255, 212);
+      border-radius: 8px;
+      margin-top: 20px;
+      margin-bottom: 20px;
+      text-align: center;
+  }
+  
+  .row5 input {
+      border: 2px solid rgb(127, 255, 212);
+      border-radius: 8px;
+      margin-bottom: 20px;
+      text-align: center;
+  }
+  
+  .row6 button{
+      background-color:aquamarine;
+      border: none;
+      color: white;
+      padding: 16px 32px;
+      text-decoration: none;
+      margin: 4px 2px;
+      cursor: pointer;
+      margin-bottom: 20px;
+      margin-left: 8px;
+      border-radius: 8px;
+  }
+  
+  .row8 input {
+      border: 2px solid rgb(127, 255, 212);
+      border-radius: 8px;
+      margin-bottom: 20px;
+      margin-left: 25px;
+      text-align: center;
+  }
+  
+  .row9 input {
+      border: 2px solid rgb(127, 255, 212);
+      border-radius: 8px;
+      margin-bottom: 20px;
+      margin-left: 25px;
+      text-align: center;
+  }
+  
+  .row10 input {
+      border: 2px solid rgb(127, 255, 212);
+      border-radius: 8px;
+      margin-bottom: 20px;
+      margin-left: 25px;
+      text-align: center;
+  }
+  
+  .row11 input {
+      border: 2px solid rgb(127, 255, 212);
+      border-radius: 8px;
+      margin-bottom: 20px;
+      margin-left: 25px;
+      text-align: center;
+  }
+  
+  .row12 input {
+      border: 2px solid rgb(127, 255, 212);
+      border-radius: 8px;
+      margin-bottom: 20px;
+      margin-left: 25px;
+      text-align: center;
+  }
+  
+  .row13 input {
+      border: 2px solid rgb(127, 255, 212);
+      border-radius: 8px;
+      margin-bottom: 20px;
+      margin-left: 25px;
+      text-align: center;
+  }
+  
+  .row14 button{
+      background-color:aquamarine;
+      border: none;
+      color: white;
+      padding: 16px 32px;
+      text-decoration: none;
+      margin: 4px 2px;
+      cursor: pointer;
+      margin-bottom: 20px;
+      margin-left: 28px;
+      border-radius: 8px;
+  }
+  
+  footer {
+      text-align: center;
+      margin-top: 40px;
+      font-weight: bold;
+  }
+  </style>
+  
